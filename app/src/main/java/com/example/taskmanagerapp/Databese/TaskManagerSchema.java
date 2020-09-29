75c146eb067c36df4c2b0f892001165eb389d825
FatemeAfshar20 <uttab4760@gmail.com>
2020-09-28 21:27:22 -0700

    adding TaskManagerSchema

---
+++ app/src/main/java/com/example/taskmanagerapp/Databese/TaskManagerSchema.java
@@ -0,0 +1,33 @@
+package com.example.taskmanagerapp.Databese;
+
+public final class TaskManagerSchema {
+    public static final String NAME="task_manager.db";
+
+    public static final class User{
+        public static final String NAME="userTable";
+
+        public static final class UserColumns{
+            public static final String ID="id";
+            public static final String UUID="uuid";
+            public static final String USERNAME="username";
+            public static final String PASSWORD="password";
+            public static final String MEMBERSHIP="membership";
+            public static final String TASK="task";
+        }
+    }
+
+    public static final class Task{
+        public static final String NAME="taskTable";
+
+        public static final class TaskColumns{
+            public static final String ID="id";
+            public static final String UUID="uuid";
+            public static final String TITLE="title";
+            public static final String CONTENT="content";
+            public static final String DATE="date";
+            public static final String TIME="time";
+            public static final String STATE="state";
+        }
+
+    }
+}
